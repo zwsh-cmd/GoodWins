3a819fa1a02711cb0731099d4f8ca21b14535c0d
zwsh-cmd <ziwush@gmail.com>
2026-01-18 20:07:07 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'goodwins-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './main.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安裝時快取檔案
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 讀取時從快取拿資料 (讓離線也能用)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
